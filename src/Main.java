06e6227a33c9867d5c76564ddf24e375c5ebbfe1
Sandry1998 <slopez@ceatformacion.com>
2025-02-04 20:54:03 +0100

    Segundo commit

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,15 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        //variables.....pueden cambiar su valor durante la ejecuccion del programa
+        //CONSTANTES....son valores que guardan valores que no cambiaran durante la ejecuccion del programa
+        final byte MAYOREDAD=18;
+        final byte IVAGENERAL=21;
+        byte edad=25;
+        System.out.println("La edad es " + (MAYOREDAD+edad));
+        //MAYOREDAD=16 //error ->es una constante que no se puede cambiar su valor
+
+
         //Operadores Matematicas
 
     }
